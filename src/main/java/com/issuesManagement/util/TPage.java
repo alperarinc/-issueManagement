effbcd4b9a4bf03767d2e0c2f6fdf19ee1564ad8
redoxkali <alperarinc@hotmail.com>
2022-05-03 18:08:30 +0300

    Pagination Bug Fix

--- src/main/java/com/issuesManagement/util/TPage.java
+++ src/main/java/com/issuesManagement/util/TPage.java
@@ -1,10 +1,15 @@
 package com.issuesManagement.util;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 
 import java.util.List;
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TPage<T> {
 
     private int number;
